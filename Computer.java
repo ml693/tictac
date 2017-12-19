1e86344af5834575358b2ecc692a0359ebbec0e6
Marius <m.latinis@gmail.com>
2017-12-19 12:29:52 +0200

    Changing approximation function to avoid overfitting.

--- Computer.java
+++ Computer.java
@@ -1,152 +1,184 @@
 package tictac;
 
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.Writer;
 import java.util.ArrayList;
 import java.util.Random;
 import java.util.Scanner;
 
-class Computer {
-	private double[][] weights = new double[Position.HEIGHT][Position.WIDTH];
+class Computer
+{
+    private double[][] weights = new double[Position.HEIGHT][Position.WIDTH];
 
-	private static final double MAX_REWARD = 1000000.0;
+    private static final double WIN_REWARD = 1.0;
+    private static final double LOSS_REWARD = 0.0;
 
-	private static final double LAMBDA = 0.98;
+    private static final double LAMBDA = 0.98;
 
-	private static final double RANDOM_MOVE_PROBABILITY = 0.1;
+    private static final double RANDOM_MOVE_PROBABILITY = 0.1;
 
-	private static final int TRAINING_AMOUNT = 10;
-	
-	private Random random = new Random();
+    private static final int TRAINING_AMOUNT = 10;
+    
+    Random random = new Random();
 
-	Computer(String fileName) {
-		try {
-		    Scanner scanner = new Scanner(new File(fileName));
-		    int i = 0;
-		    while (scanner.hasNextInt()) {
-			weights[i / Position.WIDTH][i % Position.WIDTH] = 
-				scanner.nextInt();
-			i++;
-		    }
-		} catch (Exception exception) { System.out.println("Exception!"); }
-	}
-
-	Computer() {
-		this("./tictac/NeuralNetwork.txt");
-	}
-
-	public double evaluate(Position position) {
-		Position adjustedPosition = new Position(position);
-		if (!position.whiteMovedLast()) {
-			for (int row = 0; row < Position.HEIGHT; row++) {
-				for (int col = 0; col < Position.WIDTH; col++) {
-					if (position.board[row][col] == Position.X) {
-						adjustedPosition.board[row][col] = Position.O;
-					}
-					if (position.board[row][col] == Position.O) {
-						adjustedPosition.board[row][col] = Position.X;
-					}		
-				}
-			}
+    Computer(String fileName)
+    {
+        try {
+            Scanner scanner = new Scanner(new File(fileName));
+            for (int row = 0; row < Position.HEIGHT; row++) {
+		for (int col = 0; col < Position.WIDTH; col++) {
+			weights[row][col] = scanner.nextDouble();
 		}
-		
-		int gameResult = adjustedPosition.gameResult();
-		if (gameResult == Position.WHITE_WON) {
-			return MAX_REWARD;
-		}
-		if (gameResult == Position.BLACK_WON) {
-			return -MAX_REWARD;
-		}
-		double reward = 0;
-		for (int row = 0; row < Position.HEIGHT; row++) {
-			for (int col = 0; col < Position.WIDTH; col++) {
-			    reward += weights[row][col] * ((double) position.board[row][col]);
-			}
-		}
-		return reward;
-	}
+	    }
+	    scanner.close();
+        } catch (IOException exception) {
+            throw new RuntimeException(exception);
+        }
+    }
 
-	public Position makeBestMove(Position currentPosition) {
-		ArrayList<Position> positions = currentPosition.nextPositions();
-		Position nextPosition = null;
-		double bestScore = -MAX_REWARD;
-		for (Position position : positions) {
-			double score = evaluate(position);
-			if (score > bestScore) {
-				nextPosition = position;
-				bestScore = score;
-			}
-		}
-		if (nextPosition == null) {
-			throw new RuntimeException("Should be a valid move");
-		} else {
-			return nextPosition;
-		}
-	}
+    Computer()
+    {
+        this("./tictac/NeuralNetwork.txt");
+    }
 
-	public Position makeRandomMove(Position currentPosition) {
-		ArrayList<Position> nextPositions = currentPosition.nextPositions();
-		return nextPositions.get(random.nextInt(nextPositions.size()));
-	}
+    private static double functionF(double x) {
+        return x > 0 ? x + 1 : 1 / (1 - x);
+    }
 
-	private void adjustWeights(double nextEvaluation, double currentEvaluation,
-				   ArrayList<Position> history) {
-		double lambdaToI = 1;
-		double diff = nextEvaluation - currentEvaluation;
-		for (int i = 0; i < history.size(); i++) {
-			for (int row = 0; row < Position.HEIGHT; row++) {
-				for (int col = 0; col < Position.WIDTH; col++) {
-					weights[row][col] += diff * lambdaToI * 
-						history.get(history.size() - 1 - i).board[row][col];
-				}
-			}
-			lambdaToI *= LAMBDA;
-		}
-	}
+    private static double fDerivative(double x) {
+        return x > 0 ? 1 : 1 / (1 - x) * (1 - x);
+    }
 
-	private void printWeightsToFile(String fileName) {
-		try {
-			File file = new File(fileName);
-			FileWriter writer = new FileWriter(file);
-			for (int row = 0; row < Position.HEIGHT; row++) {
-				for (int col = 0; col < Position.WIDTH; col++) {
-					writer.write(weights[row][col] + "\n");
-				}
-			}
-			writer.close();
-		} catch (IOException exception) {
-			throw new RuntimeException(exception);
-		}
-	}
+    public double evaluate(Position position)
+    {
+        Position adjustedPosition = new Position(position);
+        if (!position.whiteMovedLast()) {
+            for (int row = 0; row < Position.HEIGHT; row++) {
+                for (int col = 0; col < Position.WIDTH; col++) {
+                    if (position.board[row][col] == Position.X) {
+                        adjustedPosition.board[row][col] = Position.O;
+                    }
+                    if (position.board[row][col] == Position.O) {
+                        adjustedPosition.board[row][col] = Position.X;
+                    }
+                }
+            }
+        }
 
-	// Call to train the model
-	public static void main(String args[]) {
-		for (int i = 0; i < TRAINING_AMOUNT; i++) {
-			Computer white = new Computer("./tictac/newWeights.txt");
-			Computer black = new Computer("./tictac/newWeights.txt");
-			ArrayList<Position> history = new ArrayList<Position>();
-			Position position = new Position();
-			while (position.gameResult() == Position.UNKNOWN) {
-				history.add(position);
-				int randomNumber = white.random.nextInt(100);
-				position = (randomNumber >= 10) ? white.makeBestMove(position) :
-					white.makeRandomMove(position);
-				double currentEvaluation = white.evaluate(position);
-				double nextEvaluation;
-				if (position.gameResult() == Position.WHITE_WON) {
-					nextEvaluation = MAX_REWARD;
-				} else {
-					position = black.makeBestMove(position);
-					if (position.gameResult() == Position.BLACK_WON) {
-						nextEvaluation = -MAX_REWARD;
-					} else {
-						nextEvaluation = white.evaluate(position);
-					} 
-				}
-				white.adjustWeights(nextEvaluation, currentEvaluation, history);
-			}
-			white.printWeightsToFile("./tictac/newWeights" + i);
-		}
-	}
+        int gameResult = adjustedPosition.gameResult();
+        if (gameResult == Position.WHITE_WON) {
+            return WIN_REWARD;
+        }
+        if (gameResult == Position.BLACK_WON) {
+            return LOSS_REWARD;
+        }
+        return 1 / (1 + functionF(dotProduct(weights, position)));
+    }
+
+    private static double dotProduct(double[][] weights, Position position) {
+        double product = 0;
+        for (int row = 0; row < Position.HEIGHT; row++) {
+            for (int col = 0; col < Position.WIDTH; col++) {
+                product += weights[row][col] * ((double) position.board[row][col]);
+            }
+        }
+	return product;
+    }
+
+    public Position makeBestMove(Position currentPosition)
+    {
+        ArrayList<Position> positions = currentPosition.nextPositions();
+        Position nextPosition = null;
+        double bestScore = -1.0;
+        for (Position position : positions) {
+            double score = evaluate(position);
+	    if (score > bestScore) {
+                nextPosition = position;
+                bestScore = score;
+            }
+        }
+        if (nextPosition == null) {
+            throw new RuntimeException("Should be a valid move");
+        } else {
+            return nextPosition;
+        }
+    }
+
+    public Position makeRandomMove(Position currentPosition)
+    {
+        ArrayList<Position> nextPositions = currentPosition.nextPositions();
+        return nextPositions.get(random.nextInt(nextPositions.size()));
+    }
+
+    private void adjustWeights(double nextEvaluation, double currentEvaluation,
+                               ArrayList<Position> history)
+    {
+        double lambdaToI = 1;
+        double diff = nextEvaluation - currentEvaluation;
+        for (int i = 0; i < history.size(); i++) {
+	    int k = history.size() - 1 - i;
+            for (int row = 0; row < Position.HEIGHT; row++) {
+                for (int col = 0; col < Position.WIDTH; col++) {
+		    double product = dotProduct(weights, history.get(k));
+		    double factor = -1 * fDerivative(product) / ((1 + functionF(product)) * 
+                                                                (1 + functionF(product)));
+                    weights[row][col] += diff * lambdaToI * factor * history.get(k).board[row][col];
+                }
+            }	
+            lambdaToI *= LAMBDA;
+        }
+    }
+
+    private void printWeightsToFile(String fileName)
+    {
+        try {
+            File file = new File(fileName);
+            FileWriter writer = new FileWriter(file);
+            for (int row = 0; row < Position.HEIGHT; row++) {
+                for (int col = 0; col < Position.WIDTH; col++) {
+                    writer.write(weights[row][col] + "\n");
+                }
+            }
+            writer.close();
+        } catch (IOException exception) {
+            throw new RuntimeException(exception);
+        }
+    }
+
+    // Call to train the model
+    public static void main(String args[])
+    {
+        for (int i = 0; i < TRAINING_AMOUNT; i++) {
+            System.out.println("Iteration nr. " + i);
+
+	    Computer white = new Computer("./tictac/newWeights.txt");
+            Computer black = new Computer("./tictac/newWeights.txt");
+            ArrayList<Position> history = new ArrayList<Position>();
+            Position position = new Position();
+
+            while (position.gameResult() == Position.UNKNOWN) {
+                history.add(position);
+                double currentEvaluation = white.evaluate(position);
+		int randomNumber = white.random.nextInt(100);
+                position = (randomNumber >= 10) ? white.makeBestMove(position) :
+                           white.makeRandomMove(position);
+                double nextEvaluation;
+                if (position.gameResult() == Position.WHITE_WON) {
+                    nextEvaluation = WIN_REWARD;
+                } else {
+                    position = black.makeBestMove(position);
+                    if (position.gameResult() == Position.BLACK_WON) {
+                        nextEvaluation = LOSS_REWARD;
+                    } else {
+                        nextEvaluation = white.evaluate(position);
+                    }
+                }
+                white.adjustWeights(nextEvaluation, currentEvaluation, history);
+            }
+            white.printWeightsToFile("./tictac/newWeights.txt");
+        }
+    }
 }
--- Play.java
+++ Play.java
@@ -11,66 +11,73 @@ import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
-class Play {
-	private static Computer computer = new Computer();
-	private static Position position = new Position();
-	private static JButton[][] buttons = new JButton[Position.HEIGHT][Position.WIDTH];
+class Play
+{
+    private static Computer computer = new Computer();
+    private static Position position = new Position();
+    private static JButton[][] buttons = new
+        JButton[Position.HEIGHT][Position.WIDTH];
 
-	public static void colorBoard(Position position) {
-		for (int row = 0; row < Position.HEIGHT; row++) {
-			for (int col = 0; col < Position.WIDTH; col++) {
-				if (position.board[row][col] == Position.X) {
-					ImageIcon xIcon = new ImageIcon("./tictac/x.png");
-					buttons[row][col].setIcon(xIcon);
-				}
-				if (position.board[row][col] == Position.O) {
-					ImageIcon oIcon = new ImageIcon("./tictac/o.jpg");
-					buttons[row][col].setIcon(oIcon);
-				}
-			}
-		}
-	}
+    public static void colorBoard(Position position)
+    {
+        for (int row = 0; row < Position.HEIGHT; row++) {
+            for (int col = 0; col < Position.WIDTH; col++) {
+                if (position.board[row][col] == Position.X) {
+                    ImageIcon xIcon = new ImageIcon("./tictac/x.png");
+                    buttons[row][col].setIcon(xIcon);
+                }
+                if (position.board[row][col] == Position.O) {
+                    ImageIcon oIcon = new ImageIcon("./tictac/o.jpg");
+                    buttons[row][col].setIcon(oIcon);
+                }
+            }
+        }
+    }
 
-	public static void main(String[] args) {
-		JFrame frame = new JFrame("Let's play!");
-		frame.setSize(800, 800);
-		frame.setVisible(true);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-		JPanel panel = new JPanel();
-		panel.setLayout(new GridLayout(Position.HEIGHT, Position.WIDTH));
-		for (int i = 0; i < Position.HEIGHT * Position.WIDTH; i++) {
-			JButton button = new JButton();
-			final int row = i / Position.WIDTH;
-			final int col = i % Position.WIDTH;
-			buttons[row][col] = button;
-			button.addActionListener(new ActionListener() {
-				@Override
-				public void actionPerformed(ActionEvent e) {
-					if (position.board[row][col] == Position.EMPTY) {
-						position.board[row][col] = Position.X;
-						colorBoard(position);
-						int gameResult = position.gameResult();
-						System.out.println("Game result is " + gameResult);
-						if (gameResult == Position.WHITE_WON) {
-							System.out.println("You won");
-							return;
-						}
-						if (gameResult == Position.DRAW) {
-							System.out.println("Draw");
-							return;
-						}
-						position = computer.makeBestMove(position);
-						colorBoard(position);
-						if (position.gameResult() == Position.BLACK_WON) {
-							System.out.println("I won");
-							return;
-						}
-					}
-				}
-			});
-			panel.add(button);
-		}
-		frame.getContentPane().add(panel);
-	}
+    public static void main(String[] args)
+    {
+        JFrame frame = new JFrame("Let's play!");
+        frame.setSize(800, 800);
+        frame.setVisible(true);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        JPanel panel = new JPanel();
+        panel.setLayout(new GridLayout(Position.HEIGHT, Position.WIDTH));
+        for (int r = 0; r < Position.HEIGHT; r++) {
+            for (int c = 0; c < Position.WIDTH; c++) {
+		final int row = r;
+		final int col = c;
+                JButton button = new JButton();
+                buttons[row][col] = button;
+                button.addActionListener(new ActionListener() {
+                    @Override
+                    public void actionPerformed(ActionEvent e) {
+                        if (position.board[row][col] == Position.EMPTY) {
+                            position.board[row][col] = Position.X;
+                            colorBoard(position);
+
+                            int gameResult = position.gameResult();
+                            if (gameResult == Position.WHITE_WON) {
+                                System.out.println("You won");
+                                return;
+                            }
+                            if (gameResult == Position.DRAW) {
+                                System.out.println("Draw");
+                                return;
+                            }
+                            position = computer.makeBestMove(position);
+                            colorBoard(position);
+                            if (position.gameResult() == Position.BLACK_WON) {
+                                System.out.println("I won");
+                                return;
+                            }
+                        }
+                    }
+                });
+                panel.add(button);
+            }
+        }
+        frame.getContentPane().add(panel);
+    }
 }
